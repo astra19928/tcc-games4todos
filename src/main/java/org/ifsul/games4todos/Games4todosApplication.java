aec70584130d33f242c93b762aa5737564b9c5fc
astra19928 <eduardokirbyfun@gmail.com>
2026-09-14 10:58:39 -0300

    spring inicializado

---
+++ src/main/java/org/ifsul/games4todos/Games4todosApplication.java
@@ -0,0 +1,13 @@
+package org.ifsul.games4todos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Games4todosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Games4todosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ifsul/games4todos/Games4todosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ifsul.games4todos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Games4todosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
